6c237151a7afb07cfa1d02251e5fa15237220181
Cassia <cassia.a.barbosa@gmail.com>
2020-10-06 17:51:19 -0300

    Update TextField

--- Sources/Formworks/FWComponents/FWSingleLine/FWSingleLineComponent.swift
+++ Sources/Formworks/FWComponents/FWSingleLine/FWSingleLineComponent.swift
@@ -12,17 +12,17 @@ final class FWSingleLineComponent: UIViewController {
 	// MARK: Properties
 	@ManualLayout private var textField: FWTextField
 	
-//	@ManualLayout private var titleLabel: FWLabel
-//
-//	@ManualLayout private var requiredLabel: FWLabel
-//
-//	@ManualLayout private var titleContainerView: UIView
-//
-//	@ManualLayout private var descriptionLabel: FWLabel
-//
-//	@ManualLayout private var errorLabel: FWLabel
-//
-//	@ManualLayout private var layoutStackView: UIStackView
+	@ManualLayout private var titleLabel: FWLabel
+	
+	@ManualLayout private var requiredLabel: FWLabel
+	
+	@ManualLayout private var titleContainerView: UIView
+	
+	@ManualLayout private var descriptionLabel: FWLabel
+	
+	@ManualLayout private var errorLabel: FWLabel
+	
+	@ManualLayout private var layoutStackView: UIStackView
 	
 	private let viewModel: FWSingleLineViewModel
 	
@@ -61,14 +61,14 @@ final class FWSingleLineComponent: UIViewController {
 	}
 	
 	private func setUpHeader() {
-//		titleLabel.text = "One-line textfield One-line textfield One-line textfield One-line textfield"
-//		titleLabel.style(.title)
-//
-//		requiredLabel.text = "Required"
-//		requiredLabel.style(.required)
-//
-//		descriptionLabel.text = "Optional description for this field, Optional description for this field, Optional description for this field"
-//		descriptionLabel.style(.description)
+		titleLabel.text = "One-line textfield One-line textfield One-line textfield One-line textfield"
+		titleLabel.style(.title)
+		
+		requiredLabel.text = "Required"
+		requiredLabel.style(.required)
+		
+		descriptionLabel.text = "Optional description for this field, Optional description for this field, Optional description for this field"
+		descriptionLabel.style(.description)
 	}
 	
 	private func setUpBody() {
@@ -78,49 +78,49 @@ final class FWSingleLineComponent: UIViewController {
 	}
 	
 	private func setUpFooter() {
-//		errorLabel.text = "If there are any errors with your input, it will appear here."
-//		errorLabel.style(.error)
+		errorLabel.text = "If there are any errors with your input, it will appear here."
+		errorLabel.style(.error)
 	}
 	
 	private func setUpStack() {
-//		layoutStackView.axis = .vertical
-//		layoutStackView.distribution = .fillEqually
+		layoutStackView.axis = .vertical
+		layoutStackView.distribution = .fillEqually
 	}
 	
 	// MARK: Layout Constraints
 	private func setUpTitleContainer() {
-//		titleContainerView.addSubview(titleLabel)
-//		titleContainerView.addSubview(requiredLabel)
+		titleContainerView.addSubview(titleLabel)
+		titleContainerView.addSubview(requiredLabel)
 		
 		NSLayoutConstraint.activate([
-//			titleLabel.widthAnchor.constraint(equalTo: titleContainerView.widthAnchor,
-//											  multiplier: ComponentSpec.SingleLine.titleWidthMultipler),
-//			titleLabel.heightAnchor.constraint(equalTo: titleContainerView.heightAnchor),
-//			titleLabel.leadingAnchor.constraint(equalTo: titleContainerView.leadingAnchor),
-//
-//			requiredLabel.widthAnchor.constraint(equalTo: titleContainerView.widthAnchor,
-//												 multiplier: ComponentSpec.SingleLine.requiredWidthMultiplier),
-//			requiredLabel.heightAnchor.constraint(equalTo: titleContainerView.heightAnchor,
-//												  multiplier: ComponentSpec.SingleLine.requiredHeightMultiplier),
-//			requiredLabel.trailingAnchor.constraint(equalTo: titleContainerView.trailingAnchor)
+			titleLabel.widthAnchor.constraint(equalTo: titleContainerView.widthAnchor,
+											  multiplier: ComponentSpec.SingleLine.titleWidthMultipler),
+			titleLabel.heightAnchor.constraint(equalTo: titleContainerView.heightAnchor),
+			titleLabel.leadingAnchor.constraint(equalTo: titleContainerView.leadingAnchor),
+			
+			requiredLabel.widthAnchor.constraint(equalTo: titleContainerView.widthAnchor,
+												 multiplier: ComponentSpec.SingleLine.requiredWidthMultiplier),
+			requiredLabel.heightAnchor.constraint(equalTo: titleContainerView.heightAnchor,
+												  multiplier: ComponentSpec.SingleLine.requiredHeightMultiplier),
+			requiredLabel.trailingAnchor.constraint(equalTo: titleContainerView.trailingAnchor)
 		])
 	}
 	
 	private func setUpLayoutStack() {
-//		layoutStackView.addArrangedSubview(titleContainerView)
-//		layoutStackView.addArrangedSubview(descriptionLabel)
-//		layoutStackView.addArrangedSubview(textField)
-//		layoutStackView.addArrangedSubview(errorLabel)
-//
-		view.addSubview(textField)
+		layoutStackView.addArrangedSubview(titleContainerView)
+		layoutStackView.addArrangedSubview(descriptionLabel)
+		layoutStackView.addArrangedSubview(textField)
+		layoutStackView.addArrangedSubview(errorLabel)
+		
+		view.addSubview(layoutStackView)
 		
 		let layoutGuides = view.layoutMarginsGuide
 		
 		NSLayoutConstraint.activate([
-			textField.centerXAnchor.constraint(equalTo: layoutGuides.centerXAnchor),
-			textField.centerYAnchor.constraint(equalTo: layoutGuides.centerYAnchor),
-			textField.widthAnchor.constraint(equalTo: layoutGuides.widthAnchor),
-			textField.heightAnchor.constraint(equalTo: layoutGuides.heightAnchor)
+			layoutStackView.centerXAnchor.constraint(equalTo: layoutGuides.centerXAnchor),
+			layoutStackView.centerYAnchor.constraint(equalTo: layoutGuides.centerYAnchor),
+			layoutStackView.widthAnchor.constraint(equalTo: layoutGuides.widthAnchor),
+			layoutStackView.heightAnchor.constraint(equalTo: layoutGuides.heightAnchor)
 		])
 	}
 	
