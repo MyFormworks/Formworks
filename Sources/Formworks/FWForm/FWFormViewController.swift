7c9bd21ff082482339f09004d1fa1ef9a9522f7c
Cassia <cassia.a.barbosa@gmail.com>
2020-10-07 17:16:47 -0300

    Update FWForm init

--- Sources/Formworks/FWForm/FWFormViewController.swift
+++ Sources/Formworks/FWForm/FWFormViewController.swift
@@ -34,6 +34,7 @@ public final class FWFormViewController: UIViewController {
         super.viewDidLoad()
         setUpCollectionView()
         setUpCollectionViewConstraints()
+		setUpViewModel()
     }
 	
 	// MARK: ViewModel setup
