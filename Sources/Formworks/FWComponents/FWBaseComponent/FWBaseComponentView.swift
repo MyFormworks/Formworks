596fe8023fac2c3e9ae8ac49f847ff63969a7d46
csfar <csf.artur@gmail.com>
2020-10-16 17:26:15 -0300

    Add correct SF Symbol to required

--- Sources/Formworks/FWComponents/FWBaseComponent/FWBaseComponentView.swift
+++ Sources/Formworks/FWComponents/FWBaseComponent/FWBaseComponentView.swift
@@ -18,7 +18,7 @@ class FWBaseComponentView: UICollectionViewCell {
     @ManualLayout private var descriptionLabel: FWLabel
     @ManualLayout private var errorMessageLabel: FWLabel
     private lazy var requiredImage: UIImageView = {
-        let imageView = UIImageView(image: UIImage(systemName: "circle.fill"))
+        let imageView = UIImageView(image: UIImage(systemName: "asterisk.circle.fill"))
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
     }()
