73ff205f06921f0e167848cc900bcb5ad187df50
EdgSgroi <edgar.sgroi@gmail.com>
2020-10-29 14:20:08 -0300

    Create FWRegexValidator class and refactor FWValidator

---
+++ Sources/Formworks/FWComponents/FWRegexValidator.swift
@@ -0,0 +1,50 @@
+//
+//  FWRegexValidator.swift
+//  Formworks
+//
+//  Created by Edgar Sgroi on 29/10/20.
+//
+
+import Foundation
+
+enum FWDefaultRegexValidator {
+    case email
+    case phonenumber
+    case cellphone
+    case max32
+}
+
+final class FWRegexValidator: FWValidator {
+    let regexs: [String]
+    
+    init(regexs: [String]) {
+        self.regexs = regexs
+    }
+    
+    init(defaultValidator: FWDefaultRegexValidator) {
+        self.regexs = FWRegexValidator.getDefaultRegex(validator: defaultValidator)
+    }
+    
+    func validate(_ content: String) -> Bool {
+        for regex in regexs {
+            let predicate = NSPredicate(format: "SELF MATCHES %@", regex)
+            if predicate.evaluate(with: content) {
+                return true
+            }
+        }
+        return false
+    }
+    
+    static private func getDefaultRegex(validator: FWDefaultRegexValidator) -> [String] {
+        switch validator {
+        case .email:
+            return ["[0-9a-z._%+-]+@[a-z0-9.-]+\\.[a-z]{2,64}"]
+        case .phonenumber:
+            return ["[0-9]{2} [0-9]{8}", "[0-9]{2} [0-9]{4}-[0-9]{4}"]
+        case .cellphone:
+            return ["[0-9]{2} [0-9]{9}", "[0-9]{2} [0-9]{5}-[0-9]{4}"]
+        case .max32:
+            return ["[A-Za-z0-9 !\"#$%&'()*+,-./:;<=>?@\\[\\\\\\]^_`{|}~]{0,32}"]
+        }
+    }
+}
--- Sources/Formworks/FWComponents/FWValidator.swift
+++ Sources/Formworks/FWComponents/FWValidator.swift
@@ -7,35 +7,7 @@
 
 import Foundation
 
-enum FWValidator {
-    case email
-    case phonenumber
-    case cellphone
-    case max32
-}
 
-extension FWValidator {
-    func regex() -> [String] {
-        switch self {
-        case .email:
-            return ["[0-9a-z._%+-]+@[a-z0-9.-]+\\.[a-z]{2,64}"]
-        case .phonenumber:
-            return ["[0-9]{2} [0-9]{8}", "[0-9]{2} [0-9]{4}-[0-9]{4}"]
-        case .cellphone:
-            return ["[0-9]{2} [0-9]{9}", "[0-9]{2} [0-9]{5}-[0-9]{4}"]
-        case .max32:
-            return ["[A-Za-z0-9 !\"#$%&'()*+,-./:;<=>?@\\[\\\\\\]^_`{|}~]{0,32}"]
-        }
-    }
-    
-    func validate(_ content: String) -> Bool {
-        let regexs = self.regex()
-        for regex in regexs {
-            let predicate = NSPredicate(format: "SELF MATCHES %@", regex)
-            if predicate.evaluate(with: content) {
-                return true
-            }
-        }
-        return false
-    }
+protocol FWValidator: AnyObject {
+    func validate(_ content: String) -> Bool
 }
