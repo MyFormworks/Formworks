ca4b9574ee1eeb3d39c76cefe4ff86259fcab3ae
Cassia <cassia.a.barbosa@gmail.com>
2020-11-20 16:12:17 -0300

    content view

--- Sources/Formworks/FWForm/FWFormViews/FWDismissHeader.swift
+++ Sources/Formworks/FWForm/FWFormViews/FWDismissHeader.swift
@@ -30,7 +30,7 @@ final class FWDismissHeader: UITableViewHeaderFooterView {
 	
 	// MARK: Setup Image Constraints
 	private func setupImageConstraints() {
-		self.addSubview(button)
+		self.contentView.addSubview(button)
 		button.setTitleColor(.systemBlue, for: .normal)
 		NSLayoutConstraint.activate([
 			button.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -5),
