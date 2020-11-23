a168af1a5c44370fbb8436cb258284e0c9ecdd01
EdgSgroi <edgar.sgroi@gmail.com>
2020-11-23 17:32:09 -0300

    Change submit button

--- Sources/Formworks/FWForm/FWFormViews/FWFormSubmitTableCell.swift
+++ Sources/Formworks/FWForm/FWFormViews/FWFormSubmitTableCell.swift
@@ -36,12 +36,12 @@ final class FWFormSubmitTableCell: UITableViewCell {
         contentView.addSubview(submitLabel)
 
         NSLayoutConstraint.activate([
+            self.heightAnchor.constraint(equalToConstant: 100),
+            
 			submitLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
             submitLabel.topAnchor.constraint(equalTo: contentView.topAnchor),
             submitLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-			submitLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
-            
-            submitLabel.heightAnchor.constraint(equalToConstant: 100)
+			submitLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
         ])
     }
 }
